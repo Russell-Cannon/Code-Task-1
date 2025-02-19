c0576f1415e6623c199ad76fc71c2b6b6c6e195c
Russell Cannon <russellrcannon@gmail.com>
2025-02-18 17:22:20 -0700

    Fixed makefile

--- project/main.cpp
+++ project/main.cpp
@@ -4,11 +4,11 @@
 int main() {
     UnionFind unionFind;
 
-    // std::cout << "Enter value of N: ";
-    // int n = 0;
-    // std::cin >> n;
-    // unionFind.AddNodes(n);
-    // unionFind.PrintArrays();
+    std::cout << "Enter value of N: ";
+    int n = 0;
+    std::cin >> n;
+    unionFind.AddNodes(n);
+    unionFind.PrintArrays();
 
     return 0;
 }
